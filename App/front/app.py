54badfeadec60afc5a924f13b45b5a95df4f21fb
Francesco <romeo.francesco.2002@gmail.com>
2024-10-22 10:34:27 +0200

    from text_input to chat_input and bug fix

--- App/front/app.py
+++ App/front/app.py
@@ -35,8 +35,11 @@ if 'client_socket' not in st.session_state:
 if 'chat_history' not in st.session_state:
     st.session_state['chat_history'] = []  # Lista per memorizzare i messaggi
 
+# Titolo dell'app
+st.sidebar.title("MarIA")
+
 # Sidebar per mostrare lo storico delle chat
-st.sidebar.title("Storico delle Chat")
+st.sidebar.subheader("Storico delle Chat")
 
 # Mostra lo storico delle chat fittizie, raggruppate per data
 for day in fake_chat_history:
@@ -45,8 +48,7 @@ for day in fake_chat_history:
         st.sidebar.markdown(f"- {chat}")
     st.sidebar.markdown("---")  # Linea di separazione tra i giorni
 
-# UI principale
-st.title("MarIA - Chatbot")
+
 
 # Funzione per rendere i messaggi in stile chat WhatsApp
 def render_message(speaker, message):
@@ -55,7 +57,7 @@ def render_message(speaker, message):
             f"""
             <div style='text-align: right;'>
                 <div style='display: inline-block; background-color: #DCF8C6; padding: 10px; border-radius: 10px; margin-bottom: 10px;'>
-                    <strong>{speaker}:</strong> {message}
+                    {message}
                 </div>
             </div>
             """,
@@ -65,8 +67,8 @@ def render_message(speaker, message):
         st.markdown(
             f"""
             <div style='text-align: left;'>
-                <div style='display: inline-block; background-color: #E0F7FA; padding: 10px; border-radius: 10px; margin-bottom: 10px;'>
-                    <strong>{speaker}:</strong> {message}
+                <div style='display: inline-block; padding: 10px; margin-bottom: 10px;'>
+                    {message}
                 </div>
             </div>
             """,
@@ -81,32 +83,23 @@ def render_messages(chat_history=None):
     for speaker, message in chat_history:
         render_message(speaker, message)
 
-    # Se non ci sono messaggi, mostra un messaggio vuoto
-    if not chat_history:
-        st.markdown("Ancora nessun messaggio.")
-
-
-
-# Funzione per inviare domanda e aggiornare la UI
-def send_question():
+# Funzione per inviare domanda
+def send_question(question):
     # Se esiste una connessione e c'è una domanda
-    if st.session_state['client_socket'] and st.session_state['question']:
+    if st.session_state['client_socket'] and question:
         # Invia la domanda al server e ricevi la risposta
-        response = send_question_to_server(st.session_state['client_socket'], st.session_state['question'])
+        response = send_question_to_server(st.session_state['client_socket'], question)
 
         # Accoda la domanda e la risposta allo storico
-        st.session_state['chat_history'].append(("Utente", st.session_state['question']))
+        st.session_state['chat_history'].append(("Utente", question))
         st.session_state['chat_history'].append(("Chatbot", response))
 
-        # Cancella la domanda
-        st.session_state['question'] = ""  # Reset dell'input
 
-# Inizializza l'input della domanda
-if 'question' not in st.session_state:
-    st.session_state['question'] = ""
-
-# Mostra tutti i messaggi
-render_messages(st.session_state['chat_history'])
 
 # Invia la domanda anche se si preme "Invio"
-st.text_input("Fai una domanda al chatbot:", key="question", value=st.session_state['question'], placeholder="Scrivi qui...", on_change=send_question)
+question = st.chat_input("Fai una domanda al chatbot:")
+if question:
+    send_question(question)
+    
+    # Mostra tutti i messaggi
+    render_messages(st.session_state['chat_history'])
