2daa49333606078e32375ee90d50881511e0dc3b
Francesco <romeo.francesco.2002@gmail.com>
2024-10-31 18:48:24 +0100

    last update (for now)

--- App/AppDemo.py
+++ App/AppDemo.py
@@ -12,12 +12,11 @@ from langchain_groq import ChatGroq  # type: ignore
 from front.file_manager import load_fake_chat_history
 
 GROQ_API_KEY = st.secrets["groq_api_key"]
-not_loaded = True
 
 # Inizializzazione del file di conoscenza
 file = open('App/back/LLM/knowledge/knowBase2.txt', 'r')
 
-# Definizione della funzione per mostrare i messaggi
+# Funzione per mostrare i messaggi
 def render_message(speaker: str, message: str):
     st.markdown(
         """
@@ -39,41 +38,63 @@ def render_messages(chat_history):
     for speaker, message in chat_history:
         render_message(speaker, message)
 
-# Configurazione dell'API e del modello
-groq_api_key = GROQ_API_KEY
-model = random.choice(["gemma-7b-it", "llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma2-9b-it"])
-groq_chat = ChatGroq(groq_api_key=groq_api_key, model_name=model)
-
-# Prompt per il chatbot
-context = file.read()
-prompt = ChatPromptTemplate.from_messages(
-    [
-        SystemMessage(content=context),
-        MessagesPlaceholder(variable_name="chat_history"),
-        HumanMessagePromptTemplate.from_template("{human_input}")
-    ]
-)
-
-# Memoria della conversazione
-memory = ConversationBufferWindowMemory(k=10, memory_key="chat_history", return_messages=True)
-
-# Creazione della conversazione con LLM
-conversation = LLMChain(
-    llm=groq_chat,
-    prompt=prompt,
-    verbose=False,
-    memory=memory  # Usa la memoria condivisa per mantenere lo storico della conversazione
-)
-
 # Funzione per inviare una domanda e ottenere la risposta
 def send_question(question: str):
     if question:
-        response = conversation.predict(human_input=question)
+        response = st.session_state['conversation'].predict(human_input=question)
         st.session_state['chat_history'].append(("Utente", question))
         st.session_state['chat_history'].append(("Chatbot", response))
 
+# Inizializzazione dello storico della chat e del modello
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = []
+    st.session_state['model'] = None
+    
+    # Caricamento di chat fittizie per la sidebar
+    st.session_state['fake_chat_history'] = load_fake_chat_history()
+    
+    # Configurazione dell'API e del modello
+    model = random.choice(["gemma-7b-it", "llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma2-9b-it"])
+    st.session_state['model'] = model
+    groq_chat = ChatGroq(groq_api_key=GROQ_API_KEY, model_name=model)
+
+    # Prompt per il chatbot
+    context = file.read()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            SystemMessage(content=context),
+            MessagesPlaceholder(variable_name="chat_history"),
+            HumanMessagePromptTemplate.from_template("{human_input}")
+        ]
+    )
+
+    # Memoria della conversazione
+    memory = ConversationBufferWindowMemory(k=10, memory_key="chat_history", return_messages=True)
+
+    # Creazione della conversazione con LLM
+    st.session_state['conversation'] = LLMChain(
+        llm=groq_chat,
+        prompt=prompt,
+        verbose=False,
+        memory=memory  # Usa la memoria condivisa per mantenere lo storico della conversazione
+    )
+
+    global static_model
+    static_model = model
+
 # Sidebar per mostrare lo storico delle chat
 st.sidebar.subheader("Storico delle Chat")
+for day in st.session_state['fake_chat_history']:
+    st.sidebar.markdown(f"### {day['date']}")
+    for chat in day['chats']:
+        st.sidebar.markdown(f"- {chat}")
+    st.sidebar.markdown("---")
+
+# Pulsante per terminare la chat e resettare lo storico
+if st.sidebar.button("Termina Chat Corrente"):
+    st.session_state['chat_history'] = []
+    st.balloons()
+    st.session_state['chat_summary'] = None
 
 # Titoli e sottotitoli nell'app
 st.markdown(
@@ -94,7 +115,6 @@ st.markdown(
         -webkit-background-clip: text;
         background-size: 300% 300%;
         animation: gradientAnimation 6s ease-in-out infinite;
-        /* Added text-shadow for a subtle depth effect */
         text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
     }
     </style>
@@ -106,32 +126,12 @@ st.markdown(
 st.html("<h1 style='font-size : clamp(1rem, 5vw, 2.5rem); font-weight: 400; background-image: linear-gradient(to left, #f5f5f5, #d3d3d3); color: transparent; background-clip: text; -webkit-background-clip: text;'>Come posso aiutarti oggi?</h1>")
 
 st.warning("Questa è una demo limitata: alcune funzionalità saranno disponibili nella versione finale.", icon="⚠️")
-st.warning("Modello LLM utilizzato: " + model, icon="ℹ️")
-# Caricamento di chat fittizie per la sidebar
-fake_chat_history = load_fake_chat_history()
-
-# Mostra lo storico delle chat fittizie nella sidebar
-for day in fake_chat_history:
-    if not not_loaded:
-        break
-
-    st.sidebar.markdown(f"### {day['date']}")
-    for chat in day['chats']:
-        st.sidebar.markdown(f"- {chat}")
-    st.sidebar.markdown("---")
-not_loaded = False
-
-# Inizializzazione dello storico della chat
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-
-# Pulsante per terminare la chat e resettare lo storico
-if st.sidebar.button("Termina Chat Corrente"):
-    st.session_state['chat_history'] = []
-    st.session_state['chat_summary'] = None
+st.info("Modello utilizzato: " + st.session_state['model'])
 
 # Input della domanda e invio
 question = st.chat_input("Fai una domanda al chatbot:")
 if question:
     send_question(question)
     render_messages(st.session_state['chat_history'])
+
+
